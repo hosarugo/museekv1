2f6c2c631b0ca924b8388d1b819ef1b0c1a07c81
hosarugo <hosa.rugo@gmail.com>
2018-03-03 11:21:52 +0000

    add some devise permition  fields in sign up and update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller? #HR - signifie qu'avant toute action on exécute la méthode configure_permitted_paramters si on appelle le controller lié à Devise.
+
+  protected
+  def configure_permitted_parameters # RH - ici les actions sont inscription (:sign_up) et mise à jour du profil (:account_update). En gros, vous avez autorisé le champ fullname à l'inscription et lors de la modification du profil.
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:full_name])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :full_name, presence: true, length: {maximum: 65} #HR - Require a full field for the fullname
 end
