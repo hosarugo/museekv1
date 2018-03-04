cf1aa79fc67241b059fb8530746278c394d68ba0
hosarugo <hosa.rugo@gmail.com>
2018-03-04 14:46:20 +0000

    some chnages for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :full_name, presence: true, length: {maximum: 65} #HR - Require a full field for the fullname
+  validates :full_name, presence: false, length: {maximum: 65} #HR - Require a full field for the fullname
 end
