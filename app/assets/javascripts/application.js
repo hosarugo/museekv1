e7068e519e6221f361bfd537229141cc86441796
hosarugo <hosa.rugo@gmail.com>
2018-05-29 23:43:40 +0100

    configuration of error messages with toaster

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,19 @@
 //= require rails-ujs
-//= require jquery
+//= require jquery3
+//= require popper
 //= require jquery-ujs
 //= require jquery-ui
 //= require bootstrap-sprockets
+//= require toastr
 //= require_tree .
+
+
+// to poisiton the messages-error
+toastr.options = {
+
+ "positionClass": "toast-bottom-right"
+
+ }
 
 
 
