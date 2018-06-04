11873479c18e89279319eb390263bef91a05e358
hosarugo <hosa.rugo@gmail.com>
2018-06-04 14:42:46 +0100

    some tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :add_column_to_users
   devise_for :users,
                :path => '',
                :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}, #HR - Define new routes
