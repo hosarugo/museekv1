5a93e0029b6e28e949fe5217328c945e45fead4f
hosarugo <hosa.rugo@gmail.com>
2018-06-05 17:07:08 +0100

    ad maps on show + controller + model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180530020650) do
+ActiveRecord::Schema.define(version: 20180605144644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20180530020650) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_advertisements_on_user_id"
   end
 
@@ -70,6 +72,14 @@ ActiveRecord::Schema.define(version: 20180530020650) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string "style1"
+    t.string "style2"
+    t.string "style3"
+    t.string "instrument1"
+    t.string "instrument2"
+    t.string "instrument3"
+    t.string "instrument4"
+    t.string "instrument5"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
