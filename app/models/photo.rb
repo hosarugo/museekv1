f66577e25fb25263480606a2e437c447205afaa7
hosarugo <hosa.rugo@gmail.com>
2018-06-28 09:19:43 +0100

    some little changes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,18 @@
 class Photo < ApplicationRecord
   belongs_to :advertisement
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :medium => "500x500",  :small => "200x200", :thumb => "100x100" }
+
+
+# has_attached_file :image,
+#     :styles => lambda { |a|
+#                         { :thumb => "100x100#",
+#                           :user_defined => "#{a.instance.width}x#{a.instance.height}#" }
+#                           { :small => "300x300#",
+#                           :user_defined => "#{a.instance.width}x#{a.instance.height}#" }
+#                           { :medium => "500x500#",
+#                           :user_defined => "#{a.instance.width}x#{a.instance.height}#" }
+#                       }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
